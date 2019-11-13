ec3742ea6c361c813aca22537ae2bf3513b54ec1
ekpliska <pliska.ek@gmail.com>
2019-11-13 23:05:06 +0300

    added: LoginForm module

---
+++ src/modules/LoginForm/components/LoginForm.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Form, Icon, Input } from 'antd';
+import { Button, Panel } from '../../../components';
+import { Link } from 'react-router-dom';
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <div>
+                <div className="auth__top">
+                    <h2>Войти в аккуант</h2>
+                    <p>Для входа используйте форму входа</p>
+                </div>
+                <Panel>
+                    <Form onSubmit={() => { }} className="login-form">
+                        <Form.Item validateStatus="success" hasFeedback>
+                            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                placeholder="Username" size="large" />
+
+                        </Form.Item>
+                        <Form.Item>
+                            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                type="password" placeholder="Password" size="large" />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button className="button__large" type="primary" size="large">Войти</Button>
+                        </Form.Item>
+                        <Link className="auth__register-link" to="/signup">Зарегистрироваться</Link>
+                    </Form>
+                </Panel>
+
+            </div>
+        )
+    }
+
+}
+
+const WrappedNormalLoginForm = Form.create({ name: 'auth' })(LoginForm);
+
+export default WrappedNormalLoginForm;
--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import { Panel, Button } from '../../components';
+import { Route } from 'react-router-dom';
+import { LoginForm } from '../../modules';
 import './Auth.scss';
 
 const Auth = () => {
+
     return (
         <section className="auth">
             <div className="auth__content">
-                <div className="auth__top">
-                    <h2>Войти в аккуант</h2>
-                    <p>Для входа используйте форму входа</p>
-                </div>
-                <Panel className="">
-                    <Button className="button__large" type="primary" size="large">Click</Button>
-                </Panel>
+                <Route exact path="/" component={LoginForm} />
+                <Route exact path="/signup" render={() => <h2>signup</h2>} />
             </div>
         </section>
     )
