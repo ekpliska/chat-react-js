bda38a231dd69e348ba60bfe61fc5508a8ed07c9
ekpliska <pliska.ek@gmail.com>
2019-11-17 00:19:28 +0300

    changed: Message component

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import ru from 'date-fns/locale/ru';
 import './Message.scss';
+import readedSvg from '../../asset/readed.svg';
+import noRreadedSvg from '../../asset/noreaded.svg';
 
-const Message = ({ photo, user, text, date, incoming }) => {
+const Message = ({ photo, user, text, date, incoming, isReaded }) => {
     return (
-        <div className="message">
-            <div className="message__photo">
-                <img src={photo} alt={`Photo ${user.fullName}`} />
-            </div>
+        <div className={classNames('message', { 'message--incoming' : !incoming })}>
             <div className="message__content">
-                <div className="message__bubble">
-                    <p className="message__text">
-                        {text}
-                    </p>
+                {
+                    !incoming && isReaded 
+                        ? <img className="message__icon-readed" src={readedSvg} alt="Readed icon" />
+                        : <img className="message__icon-readed message__icon-readed--no" src={noRreadedSvg} alt="NoReaded icon" />
+                }
+                <div className="message__photo">
+                    <img src={photo} alt={`Photo ${user.fullName}`} />
                 </div>
-                <span className="message__date">
-                    {formatDistanceToNow(date, {includeSeconds: true, addSuffix: true, locale: ru})}
-                </span>
+                <div className="message__info">
+                    <div className="message__bubble">
+                        <p className="message__text">
+                            {text}
+                        </p>
+                    </div>
+                    <span className="message__date">
+                        {formatDistanceToNow(date, {includeSeconds: true, addSuffix: true, locale: ru})}
+                    </span>
+                </div>
             </div>
         </div>
     )
@@ -29,6 +39,7 @@ Message.defaultProps = {
 }
 
 Message.propTypes = {
+    className: PropTypes.string,
     photo: PropTypes.string,
     text: PropTypes.string,
     date: PropTypes.string,
--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -9,6 +9,12 @@ const Chat = () => {
                 text="__text" 
                 date={new Date(2019, 9, 30)} 
                 incoming={true} />
+            <Message 
+                photo="https://static-cdn.jtvnw.net/jtv_user_pictures/61763701-db95-45b3-8622-25c1ca38e1c1-profile_image-70x70.png" 
+                text="__text 2" 
+                date={new Date(2019, 9, 30)} 
+                incoming={false} 
+                isReaded={false} />
         </section>
     )
 }
