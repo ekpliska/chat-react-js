ab20a58df6e0f52458321aee6997dded74e0849f
ekpliska <pliska.ek@gmail.com>
2019-12-24 21:53:16 +0300

    fixed: Chats page

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Button } from './Button';
 export { default as Panel } from './Panel';
 export { default as Message } from './Message';
+export { default as Messages } from './Messages';
 export { default as DateTime } from './DateTime';
 export { default as CheckMessIcon } from './CheckMessIcon';
 export { default as DialogItem } from './DialogItem';
