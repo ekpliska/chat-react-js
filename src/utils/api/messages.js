031280734369054a0531952b663c1accaa9f5695
ekpliska <pliska.ek@gmail.com>
2020-02-09 14:03:36 +0300

    added: removing to user own message at the frontend

--- src/utils/api/messages.js
+++ src/utils/api/messages.js
@@ -2,7 +2,7 @@ import { axios } from '../../core';
 
 export default {
     getMessagesDialog: (dialogId) => axios.get(`/messages?dialog=${dialogId}`),
-    removeMessage: (messageId) => axios.delete(`/messages?id=${messageId}`),
+    removeMessage: (messageId) => axios.delete(`/messages/${messageId}`),
     send: (dialogId, messageText) => axios.post('/messages', {
         dialog_id: dialogId,
         text: messageText
